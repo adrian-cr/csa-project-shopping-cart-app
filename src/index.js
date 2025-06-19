6803363ca1fbc348b85a34b0ef0e65ebf7a63192
Adrian Camacho <acr77frb@gmail.com>
2025-06-18 22:13:05 -0600

    Modified router basename prop.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <CartProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.REACT_APP_BASE_URL}>
         <App />
       </BrowserRouter>
     </CartProvider>
